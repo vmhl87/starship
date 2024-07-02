525e314f6145170de3c864c91d066144a36a57ee
Vincent Loh <vincentmloh87@gmail.com>
2024-07-02 01:38:09 -0400

    more reorganization ig

--- build/source/config.py
+++ build/source/config.py
@@ -2,22 +2,4 @@ def title():
     return "Blog"
 
 def version():
-    f = open("state.txt")
-    v = int(f.read().split('\n')[1].split(' ')[0])
-    f.close()
-    return v
-
-def postcount():
-    f = open("state.txt")
-    v = int(f.read().split('\n')[0].split(' ')[0])
-    f.close()
-    return v
-
-def setpostcount(p):
-    f = open("state.txt")
-    a = f.read().split['\n']
-    f.close()
-    a[0] = str(p) + " :: pages"
-    f = open("state.txt", "w")
-    for b in a: f.write(b)
-    f.close()
+    return 1  # DO NOT CHANGE
