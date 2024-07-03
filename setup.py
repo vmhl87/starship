f51913166496e623eec91f9fe444cf2d99b80bc1
Vincent Loh <vincentmloh87@gmail.com>
2024-07-02 20:20:24 -0400

    confirmed v1.2 functional

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ print("Starship Quick Setup\n")
 
 title = input("Enter blog title (can be changed later): ")
 
-f = open("build/source/title.cfg")
+f = open("build/source/title.cfg", "w")
 f.write(title)
 f.close()
