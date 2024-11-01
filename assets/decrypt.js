7f9834e7ce3dc8c8f354896c76a5a2cb296c49ee
Vincent Loh <vincentmloh87@gmail.com>
2024-11-01 09:55:14 -0400

    post

--- assets/decrypt.js
+++ assets/decrypt.js
@@ -1,7 +1,43 @@
-function decrypt(ciphertext, k) {
+function raw_decrypt(ciphertext, k) {
 	const key = CryptoJS.enc.Utf8.parse(k.padEnd(16, '0'));
 	const decrypted = CryptoJS.AES.decrypt(ciphertext, key, {mode:CryptoJS.mode.ECB});
 	return decrypted.toString(CryptoJS.enc.Utf8);
+}
+
+function splay(str, base, idx) {
+	let res = "";
+
+	for(let i=idx; i<str.length; i+=base) {
+		res += str[i];
+	}
+
+	return res;
+}
+
+function weave(strings, base) {
+	let res = "";
+
+	for(let idx=0, pos=0; pos<strings[idx].length;) {
+		res += strings[idx][pos];
+		++idx;
+		if(idx == base) {
+			idx = 0;
+			++pos;
+		}
+	}
+
+	return res;
+}
+
+function decrypt(ciphertext, key) {
+	let keys = 1 + Math.floor(key.length / 16),
+		decrypted = [];
+
+	for(let i=0; i<keys; ++i) {
+		decrypted.push(raw_decrypt(splay(ciphertext, keys, i), splay(key, keys, i)));
+	}
+
+	return weave(decrypted, keys);
 }
 
 function trypass(key, root) {
--- build/encrypt.py
+++ build/encrypt.py
@@ -2,19 +2,41 @@ import base64
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad,unpad
 
-#AES ECB mode without IV
-
-def encrypt(raw, k):
+def raw_encrypt(raw, k):
     key = k.ljust(16, '0')
     raw = pad(raw.encode(),16)
     cipher = AES.new(key.encode('utf-8'), AES.MODE_ECB)
     return base64.b64encode(cipher.encrypt(raw)).decode("utf-8", "ignore")
 
-def decrypt(enc, k):
-    key = k.ljust(32, '0')
-    enc = base64.b64decode(enc)
-    cipher = AES.new(key.encode('utf-8'), AES.MODE_ECB)
-    return unpad(cipher.decrypt(enc),16).decode("utf-8", "ignore")
+def splay(s, base, idx):
+    res = ""
+
+    for i in range(idx, len(s), base):
+        res += s[i]
+
+    return res
+
+def weave(s, base):
+    res = ""
+
+    idx, pos = 0, 0
+    while pos < len(s[idx]):
+        res += s[idx][pos]
+        idx += 1
+        if idx == base:
+            idx = 0
+            pos += 1
+
+    return res
+
+def encrypt(plaintext, key):
+    keys = 1 + len(key) // 16
+    encrypted = []
+
+    for i in range(keys):
+        encrypted.append(raw_encrypt(splay(plaintext, keys, i), splay(key, keys, i)))
+
+    return weave(encrypted, keys)
 
 def usage(x=None):
     if x == "preview":
